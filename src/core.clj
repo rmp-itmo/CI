0ecfce22c459bf59d9bec13042a1dfee84517cfe
dudosyka <a.hatson22@gmail.com>
2024-11-04 20:17:00 +0300

    configuration via cmd args added

--- src/core.clj
+++ src/core.clj
@@ -1,4 +1,4 @@
-(ns listener
+(ns core
   (:gen-class)
   (:require [compojure.core :refer [POST routes]]
             [ring.adapter.jetty :as jetty]
@@ -9,29 +9,35 @@
             [clojure.string :as str]
             [clojure.java.shell :refer [sh]]))
 
-(def scripts-path "/home/dudosyka/lumin/listener-scripts/")
-
 (def app-routes
   (apply routes
          (concat '()
                  [(POST "/ci" request
-                        (let [name (-> request :params :repository :name)
+                        (let [{:keys [scripts-path valid-ref]} (:configuration request)
+                              name (-> request :params :repository :name)
                               ref (-> request
                                       :params
                                       :ref
                                       (str/split #"/")
                                       (last))]
                           (println "New event: " ref name)
-                          (when (= ref "master")
+                          (when (= ref valid-ref)
                             (println "Run: " (str scripts-path name ".sh"))
                             (sh "bash" (str scripts-path name ".sh")))
                           (response/response "ok")))])))
 
+(defn wrap-ci-configuration
+  [handler configuration]
+  (fn [request]
+    (handler (assoc request :configuration configuration))))
 
-(defn -main []
+
+(defn -main [& args]
   (jetty/run-jetty
     (-> app-routes
+        (wrap-ci-configuration {:scripts-path (second args)
+                                :valid-ref (first args)})
         (wrap-keyword-params)
         (wrap-params)
         (wrap-json-params))
-    {:port 8082 :join? false}))
+    {:port 8097 :join? false}))
